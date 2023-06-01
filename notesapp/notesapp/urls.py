91fcabfe93043d52cc50dc94fa531596cae26c23
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-06-01 21:24:24 +0000

    crud functionality

--- notesapp/notesapp/urls.py
+++ notesapp/notesapp/urls.py
@@ -14,8 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from notes.views import NoteViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r"^note$", NoteViewSet.as_view(
+        {
+            'get': 'retrieve',
+            'post': 'create',
+            'put': 'update',
+            'patch': 'partial_update',
+            'delete': 'destroy'
+        }
+    )),
+    re_path(r"^note/all$", NoteViewSet.as_view({
+        'get': 'list'
+    })),
 ]
