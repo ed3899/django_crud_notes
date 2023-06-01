7b0428b2a546443d0b56bb6264d39c9300969422
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-06-01 20:38:24 +0000

    created notes model

--- notesapp/notes/models.py
+++ notesapp/notes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+    last_updated_on = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
